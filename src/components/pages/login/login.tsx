2e8ac49828e0603e43a87dd0f3c9a2096d08ce3b
Akhil Soman <akhil.s@TA.COM>
2022-10-30 16:12:57 +0530

    30-10-2022

--- src/components/pages/login/login.tsx
+++ src/components/pages/login/login.tsx
@@ -1,8 +1,35 @@
 import React from "react";
-
+import { Form, Button, Input, message } from "antd";
+import { useNavigate, useParams } from "react-router-dom";
+import authStore from "../../store/auth-store";
+import RoutePath from "../../global/route-paths";
 type LoginProps = {};
 
 const LoginComponent: React.FC<any> = (props: LoginProps) => {
+  const { useForm } = Form;
+  const [form] = useForm();
+  const navigate = useNavigate();
+  const LoginSession = () => {
+    form
+      .validateFields()
+      .then((values) => {
+        const data = {
+          email: values.email,
+          password: values.password,
+        };
+
+        authStore.login(data.email, data.password, (res: any) => {
+          if (res) {
+            form.resetFields();
+            navigate(RoutePath.home);
+          }
+        });
+      })
+      .catch((errorinfo) => {
+        console.log(errorinfo);
+      });
+  };
+
   return (
     <>
       <main className="main">
@@ -15,27 +42,45 @@ const LoginComponent: React.FC<any> = (props: LoginProps) => {
                   <div className="heading mb-1">
                     <h2 className="title">Log in</h2>
                   </div>
-                  <form action="#">
+                  <Form
+                    id="LoginForm"
+                    form={form}
+                    key="LoginForm"
+                    preserve={false}
+                  >
                     <label htmlFor="login-email">
                       Username or email address
                       <span className="required">*</span>
                     </label>
-                    <input
-                      type="email"
-                      className="form-input form-wide"
-                      id="login-email"
-                      required
-                    />
+
+                    <Form.Item
+                      className=""
+                      name="email"
+                      rules={[
+                        {
+                          required: true,
+                          message: "Enter your Email",
+                        },
+                        {
+                          type: "email",
+                          message: "Enter your Email Address",
+                        },
+                      ]}
+                    >
+                      <Input maxLength={70} className="form-input form-wide" />
+                    </Form.Item>
                     <label htmlFor="login-password" className="mt-1">
                       Password
                       <span className="required">*</span>
                     </label>
-                    <input
-                      type="password"
-                      className="form-input form-wide"
-                      id="login-password"
-                      required
-                    />
+                    <Form.Item
+                      name="password"
+                      rules={[
+                        { required: true, message: "Enter your Password" },
+                      ]}
+                    >
+                      <Input maxLength={70} className="form-input form-wide" />
+                    </Form.Item>
                     <div className="form-footer mb-3">
                       <div className="custom-control custom-checkbox ">
                         <input
@@ -57,10 +102,20 @@ const LoginComponent: React.FC<any> = (props: LoginProps) => {
                         Forgot Password?
                       </a>
                     </div>
-                    <button type="submit" className="btn btn-dark btn-md w-100">
-                      LOG IN
-                    </button>
-                  </form>
+
+                    <Button
+                      className="btn btn-dark btn-md w-100"
+                      form="LoginForm"
+                      key="submit"
+                      htmlType="submit"
+                      //onClick={saveSession}
+                      onClick={() => {
+                        LoginSession();
+                      }}
+                    >
+                      SIGN UP
+                    </Button>
+                  </Form>
                 </div>
               </div>
             </div>
--- src/components/pages/register/register.tsx
+++ src/components/pages/register/register.tsx
@@ -1,8 +1,40 @@
 import React from "react";
-
+import { Form, Button, Input, message } from "antd";
+import { useNavigate, useParams } from "react-router-dom";
+import RoutePath from "../../global/route-paths";
+import userStore from "../../store/user-store";
 type RegistrationProps = {};
-
 const RegistrationComponent: React.FC<any> = (props: RegistrationProps) => {
+  const { useForm } = Form;
+  const [form] = useForm();
+  const navigate = useNavigate();
+  const SignupSession = () => {
+    form
+      .validateFields()
+      .then((values) => {
+        // handleModelVisible(false);
+        const data = {
+          name: values.username,
+          email: values.email,
+          password: values.password,
+          c_password: values.confirmpassword,
+          type: 1,
+        };
+
+        userStore.addRegistration(data, (res: any) => {
+          if (res) {
+            message.success("User registration completed successfully");
+            form.resetFields();
+
+            navigate(RoutePath.login);
+          }
+        });
+      })
+      .catch((errorinfo) => {
+        console.log(errorinfo);
+      });
+  };
+
   return (
     <>
       <main className="main">
@@ -15,62 +47,120 @@ const RegistrationComponent: React.FC<any> = (props: RegistrationProps) => {
                   <div className="heading mb-1">
                     <h2 className="title">Sign up</h2>
                   </div>
-                  <form action="#">
+                  <Form
+                    id="signupForm"
+                    form={form}
+                    key="signupForm"
+                    preserve={false}
+                  >
                     <div>
                       <label htmlFor="login-email">
                         Username
                         <span className="required">*</span>
                       </label>
-                      <input
-                        type="email"
-                        className="form-input form-wide"
-                        id="login-email"
-                        required
-                      />
+                      <Form.Item
+                        className="gm-margin-bottom-sm"
+                        name="username"
+                        rules={[
+                          {
+                            required: true,
+                            message: "Enter your username",
+                          },
+                        ]}
+                      >
+                        <Input
+                          maxLength={70}
+                          className="form-input form-wide"
+                        />
+                      </Form.Item>
                     </div>
                     <div>
                       <label htmlFor="login-email" className="mt-1">
                         Email address
                         <span className="required">*</span>
                       </label>
-                      <input
-                        type="email"
-                        className="form-input form-wide"
-                        id="login-email"
-                        required
-                      />
+                      <Form.Item
+                        className="gm-margin-bottom-sm"
+                        name="email"
+                        rules={[
+                          {
+                            required: true,
+                            message: "Enter your Email",
+                          },
+                          {
+                            type: "email",
+                            message: "Enter your Email Address",
+                          },
+                        ]}
+                      >
+                        <Input
+                          maxLength={70}
+                          className="form-input form-wide"
+                        />
+                      </Form.Item>
                     </div>
                     <div>
                       <label htmlFor="login-password" className="mt-1">
                         Password
                         <span className="required">*</span>
                       </label>
-                      <input
-                        type="password"
-                        className="form-input form-wide"
-                        id="login-password"
-                        required
-                      />
+                      <Form.Item
+                        name="password"
+                        rules={[
+                          { required: true, message: "Enter your Password" },
+                        ]}
+                      >
+                        <Input
+                          maxLength={70}
+                          className="form-input form-wide"
+                        />
+                      </Form.Item>
                     </div>
                     <div>
                       <label htmlFor="login-password" className="mt-1">
                         Confirm Password
                         <span className="required">*</span>
                       </label>
-                      <input
-                        type="password"
-                        className="form-input form-wide"
-                        id="login-password"
-                        required
-                      />
+                      <Form.Item
+                        name="confirmpassword"
+                        rules={[
+                          { required: true, message: "Re-enter your password" },
+                          ({ getFieldValue }) => ({
+                            validator(_, value) {
+                              if (
+                                !value ||
+                                getFieldValue("password") === value
+                              ) {
+                                return Promise.resolve();
+                              }
+
+                              return Promise.reject(
+                                new Error("Password not matching")
+                              );
+                            },
+                          }),
+                        ]}
+                      >
+                        <Input
+                          maxLength={70}
+                          className="form-input form-wide"
+                        />
+                      </Form.Item>
                     </div>
-                    <button
-                      type="submit"
+
+                    <Button
                       className="btn btn-dark btn-md w-100 mt-4"
+                      form="signupForm"
+                      key="submit"
+                      htmlType="submit"
+                      //onClick={saveSession}
+                      onClick={() => {
+                        SignupSession();
+                      }}
                     >
                       SIGN UP
-                    </button>
-                  </form>
+                    </Button>
+                  </Form>
                 </div>
               </div>
             </div>
