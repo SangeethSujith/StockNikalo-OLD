d4a578999b65f63cbca4809df99428f8efc5d8c6
amaldm <amaldevm1998@gmail.com>
2023-05-15 08:32:23 +0530

    antd style import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import 'antd/dist/antd.css';
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 import AppRouter from "./components/routing/application-routes/application-routes";
